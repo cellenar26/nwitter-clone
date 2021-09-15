2de94c3e17f3354be1e504a0782f2fd5c5bfe644
cellenar26 <cellenar26@gmail.com>
2021-09-15 10:52:20 +0900

    using .env

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import firebase from './firebase';
 import App from "./App";
+console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
