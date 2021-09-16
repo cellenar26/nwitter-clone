1655fb1cf7b720b2de500eb46096b32144fe8c39
cellenar26 <cellenar26@gmail.com>
2021-09-16 22:01:48 +0900

    make form for nwitt

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,22 @@
+import { useState } from 'react/cjs/react.development'
+
 const Home = () => {
-    return (<span>Home</span>)
+
+    const [nweet, setNweet] = useState("")
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+    }
+
+    const onChange = (event) => {
+        event.preventDefault()
+        const { target: {value},} = event
+        setNweet(value)
+    }
+    return (<form onSubmit={onSubmit}>
+        <input value={nweet} onChange={onChange} type="text" placeholder="What 's on your mind?" maxLength={120} />
+        <input type="submit" value="Nweet" />
+    </form>)
 }
 
 export default Home
