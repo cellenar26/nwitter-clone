38d0988482fdfe42573a36cd590f127cf4399a72
cellenar26 <cellenar26@gmail.com>
2021-09-17 13:45:33 +0900

    using browser api and can see pre-image

--- src/components/App.js
+++ src/components/App.js
@@ -21,7 +21,7 @@ function App() {
   return (
     <>
       {init? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/>: "Initializing..."}
-      <footer>&copy; {new Date().getFullYear()} Nwitter-clone</footer>
+      
     </>
   );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,19 +1,19 @@
-import Nweet from 'components/Nweet';
+import Nweet from "components/Nweet";
 import { dbService } from "firebase";
 import { useEffect } from "react";
 import { useState } from "react/cjs/react.development";
 
-const Home = ({userObj}) => {
+const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
-
+  const [attachment, setAttachment] = useState("");
   const onSubmit = async (event) => {
     event.preventDefault();
 
     await dbService.collection("nweets").add({
       text: nweet,
       createdAt: Date.now(),
-      creatorId: userObj.uid
+      creatorId: userObj.uid,
     });
     setNweet("");
   };
@@ -26,27 +26,50 @@ const Home = ({userObj}) => {
     setNweet(value);
   };
 
-  
-
   useEffect(() => {
     dbService.collection("nweets").onSnapshot((snapshot) => {
-        const newArray = snapshot.docs.map((document) => ({
-            id: document.id,
-            ...document.data()
-        }))
-        setNweets(newArray)
-    })
+      const newArray = snapshot.docs.map((document) => ({
+        id: document.id,
+        ...document.data(),
+      }));
+      setNweets(newArray);
+    });
   }, []);
+
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    // browser api
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+
+  const onClearAttachment = () => setAttachment("");
 
   return (
     <>
       <form onSubmit={onSubmit}>
         <input value={nweet} onChange={onChange} type="text" placeholder="What 's on your mind?" maxLength={120} />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Nweet" />
+        {attachment && (
+          <div>
+            <button onClick={onClearAttachment}>Clear</button>
+            <img src={attachment} width="50px" height="50px" />
+          </div>
+        )}
       </form>
       <div>
         {nweets.map((nweet) => (
-          <Nweet key={nweet.id} nweetObj={nweet} isOwner={userObj.uid === nweet.creatorId}/>
+          <Nweet key={nweet.id} nweetObj={nweet} isOwner={userObj.uid === nweet.creatorId} />
         ))}
       </div>
     </>
